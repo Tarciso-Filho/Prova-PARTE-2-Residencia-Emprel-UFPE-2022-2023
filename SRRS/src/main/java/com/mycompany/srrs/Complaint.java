64bdf36fcac901de6660d7b3523bfcc4fa05ef60
Tarciso Filho <tarciso.filho@hotmail.com>
2022-06-21 18:07:01 -0300

    Inicialização da Classe Complaint

---
+++ SRRS/src/main/java/com/mycompany/srrs/Complaint.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.srrs;
+
+/**
+ *
+ * @author TFilho
+ */
+public class Complaint 
+{
+    private String descricaoQueixa, observacaoQueixa, nomeSolicitante, 
+            ruaSolicitante, compSolicitante, bairroSolicitante, 
+            cidadeSolicitante, ufSolicitante, cepSolicitante, telefoneSolicitante, 
+            emailSolicitante;
+}
